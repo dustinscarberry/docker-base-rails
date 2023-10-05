6f3b38a40f29874fdabf7647dc65c409ee6a5f0b
Dustin Scarberry <bitsnbytes1001@gmail.com>
2023-10-05 11:07:28 -0400

    Ruby versions

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ docker login
 
 # build and push docker images
 
-docker build -t dustinscarberry/rails-base:ruby3.1 --build-arg RUBY_VERSION=$ruby31Version -f images/Dockerfile .
-docker push dustinscarberry/rails-base:ruby3.1
+docker build -t dustinscarberry/rails:ruby3.1 --build-arg RUBY_VERSION=$ruby31Version -f images/Dockerfile .
+docker push dustinscarberry/rails:ruby3.1
 
-docker build -t dustinscarberry/rails-base:ruby3.2 --build-arg RUBY_VERSION=$ruby32Version -f images/Dockerfile .
-docker push dustinscarberry/rails-base:ruby3.2
+docker build -t dustinscarberry/rails:ruby3.2 --build-arg RUBY_VERSION=$ruby32Version -f images/Dockerfile .
+docker push dustinscarberry/rails:ruby3.2
